ce891dd44bbb3d43579448cecbfef8e4d8e66f34
HowtoWebDev <developedbyEmre@gmail.com>
2025-08-14 15:41:25 +0300

    starter code

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-sky-900 via-slate-950 to-emerald-900 flex flex-col items-center justify-center p-6 gap-10">
+      <h1 className="text-7xl text-white">AI Interview Coach Starter Code</h1>
+    </div>
+  );
+}
+
+export default App;
